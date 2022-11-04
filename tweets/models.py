e79fc08c73f1064700c0b9f892cf37d8dea77112
Rei-Goda <r.goda@4nonome.com>
2022-11-04 19:58:41 +0900

    models, forms の作成、TweetCrateViewの作成、htmlの更新

--- tweets/models.py
+++ tweets/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.urls import reverse
 
-# class Tweet(models.Model):
-#     pass
+from accounts.models import CustomUser
+
+
+class Tweet(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    content = models.TextField(verbose_name="内容", max_length=140)
+    created_date = models.DateTimeField(verbose_name="投稿日", auto_now_add=True)
--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,39 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    TemplateView,
+)
+
+from .forms import TweetForm
+from .models import Tweet
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
+    model = Tweet
     template_name = "tweets/home.html"
+
+
+class TweetCreateView(LoginRequiredMixin, CreateView):
+    template_name = "tweets/tweet_create.html"
+    model = Tweet
+    form_class = TweetForm
+    success_url = reverse_lazy("tweets:home")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        tweet = form.save(commit=False)
+        tweet.user - self.request.user
+        tweet.save()
+        return response
+
+
+class TweetDetailView(LoginRequiredMixin, DetailView):
+    pass
+
+
+class TweetDeleteView(LoginRequiredMixin, DeleteView):
+    pass
