6c006270d23c47af4e9634508038bf3754b82e81
Rei-Goda <r.goda@4nonome.com>
2023-02-18 17:56:06 +0900

    ツイート機能：修正漏れの処理

--- tweets/views.py
+++ tweets/views.py
@@ -30,7 +30,6 @@ class TweetDetailView(LoginRequiredMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
         return context
 
 
